5a736c132513af4cdcc8748b80c12a49f87a5968
Vladyslav Samodelok <vladfux4@gmail.com>
2023-07-16 22:13:16 +0200

    Update

--- lua/plugins/tab.lua
+++ lua/plugins/tab.lua
@@ -1,4 +1,12 @@
 return {
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
